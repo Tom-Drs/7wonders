1b448b6ee9014e0a1398ca34f6784c2628704afb
Tom <50496792+Tom-Drs@users.noreply.github.com>
2022-04-01 23:14:28 +0200

    Add files via upload

---
+++ cards/factory.py
@@ -0,0 +1,73 @@
+import json
+from cards.cards import *
+
+
+def get_cards_per_age(age):
+    cards = []
+    cards_data = get_cards_data()
+    ages_data = get_ages_data()
+    if age == 1:
+        for card in ages_data.get("1").get("3 players"):
+            if cards_data.get(card).get("type") == "Raw Materials":
+                cards.append(get_raw_materials(cards_data, card, age))
+            elif cards_data.get(card).get("type") == "Manufactured Goods":
+                cards.append(get_manufactured_goods(cards_data, card, age))
+            elif cards_data.get(card).get("type") == "Civilian Structures":
+                cards.append(get_civilian_structures(cards_data, card, age))
+            elif cards_data.get(card).get("type") == "Military Structures":
+                cards.append(get_military_structures(cards_data, card, age))
+            elif cards_data.get(card).get("type") == "Scientific Structures":
+                cards.append(get_scientific_structures(cards_data, card, age))
+            elif cards_data.get(card).get("type") == "Commercial Structures":
+                cards.append(get_commercial_structures(cards_data, card, age))
+    return cards
+
+
+def get_cards_data():
+    with open("cards/cards_details.json", "r") as file:
+        return json.load(file)
+
+
+def get_ages_data():
+    with open("cards/ages_details.json", "r") as file:
+        return json.load(file)
+
+
+def get_raw_materials(cards_data, card, age):
+    production = {key: value for key, value in cards_data.get(card).get("production").items()}
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return RawMaterials(production, card, age, "brown", cost=cost)
+
+
+def get_manufactured_goods(cards_data, card, age):
+    production = {key: value for key, value in cards_data.get(card).get("production").items()}
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return ManufacturedGoods(production, age, age, "grey", cost=cost)
+
+
+def get_civilian_structures(cards_data, card, age):
+    victory_points = cards_data.get(card)["victory points"]
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return CivilianStructures(victory_points, card, age, "blue", cost=cost)
+
+
+def get_military_structures(cards_data, card, age):
+    war_points = cards_data.get(card)["war points"]
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return MilitaryStructures(war_points, card, age, "red", cost=cost)
+
+
+def get_scientific_structures(cards_data, card, age):
+    symbol = cards_data.get(card)["symbol"]
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return ScientificStructures(symbol, card, age, "green", cost=cost)
+
+
+def get_commercial_structures(cards_data, card, age):
+    effect = cards_data.get(card)["effect"]
+    cost = {key: value for key, value in cards_data.get(card).get("cost").items()}
+    return CommercialStructures(effect, card, age, "yellow", cost=cost)
+
+
+if __name__ == "__main__":
+    print(get_cards_per_age(1))
---
+++ engine.py
@@ -0,0 +1,116 @@
+from player import Player
+from pprint import pprint
+from cards.factory import get_cards_per_age
+from random import randint
+from os import path
+import pickle
+
+class GameEngine:
+
+    def __init__(self, number_player: int=3):
+        self.number_player = number_player
+        self.players = []
+        self.cards = []
+        self.current_age = 1
+        self.copy_state = 0
+        self.create_players()
+        self.cards_deposit = []
+        self.allocate_decks()
+        self.play_current_round()
+
+    def create_players(self):
+        """Method to create player objects."""
+        for player_id in range(self.number_player):
+            new_player = Player(id=player_id, hand_cards=[], engine=self,
+                                wonder=None)
+            self.players.append(new_player)
+
+    def wait_players(self):
+        """Method to call the play method for each player."""
+        for player in self.players:
+            player.play()
+
+    def receive_card(self, player_id: int, card):
+        """Method call when a player wants to play a card."""
+        player = self.get_player_by_id(player_id, self.players)
+        if player is None:
+            raise Exception("Player not found with id.")
+        if not player.is_double(card):
+            self.cards_deposit.append((card, player))
+
+    def get_player_by_id(self, player_id: int, players_list: list):
+        """Method to get player in the player list with an id."""
+        for player in players_list:
+            if player.id == player_id:
+                return player
+        return None
+
+    def play_current_round(self):
+        """Method to start the game."""
+        for i in range(7):
+            self.wait_players()
+            if len(self.cards_deposit) == self.number_player:
+                self.copy_state = self.get_copy_state()
+                for card, player in self.cards_deposit:
+                    current_player = self.get_player_by_id(player.id,
+                                                           self.copy_state)
+                    current_player.placed_cards.append(card)
+                    for card_index in range(len(player.hand_cards)):
+                        if card.name == current_player.hand_cards[
+                            card_index].name:
+                            current_player.hand_cards.pop(card_index)
+                            break
+                    current_player.print_data()
+            self.next_state()
+            self.cards_deposit = []
+            self.switch_decks()
+
+    def next_state(self):
+        """Method to transfert the state copy in the main state."""
+        self.players = []
+        for player in self.copy_state:
+            self.players.append(player)
+
+    def get_copy_state(self) -> list:
+        return [player for player in self.players]
+
+    def allocate_decks(self):
+        """Method to allocate decks for each player."""
+        cards = get_cards_per_age(self.current_age)
+        number_cards = int(len(cards) / self.number_player)
+        for player in range(self.number_player):
+            for _ in range(number_cards):
+                random = randint(0, len(cards) - 1)
+                self.get_player_by_id(player, self.players).hand_cards.append(
+                    cards[random])
+                print(cards[random])
+                cards.pop(random)
+
+    def switch_decks(self):
+        """Method to switching decks between players."""
+        decks = []
+        if self.current_age % 2 == 1:
+            for player_index in range(len(self.players) - 1):
+                decks.append(self.players[player_index].hand_cards)
+            decks.insert(0, self.players[-1].hand_cards)
+        if self.current_age % 2 == 0:
+            for player_index in range(len(self.players) - 1, 0, -1):
+                decks.insert(0, self.players[player_index].hand_cards)
+            decks.append(self.players[0].hand_cards)
+        for index in range(len(self.players)):
+            self.players[index].hand_cards = decks[index]
+
+    def can_put_card(self, card, player):
+        """Method  to verify if a player can put a card."""
+        resource = player.get_all_resources()
+        cost = card.cost
+        pass  # comparer les ressources avec le prix de la carte
+        
+    def create_backup(self):
+        index = 0
+        while path.exists(f"backups/state{index}.pickle"):
+            index += 1
+        with open(f"backups/state{index}.pickle", "wb") as file:
+            game = pickle.dump(self, file)
+        print(f"Etat copie dans le fichier backups/state{index}.pickle")
+        exit(0)
